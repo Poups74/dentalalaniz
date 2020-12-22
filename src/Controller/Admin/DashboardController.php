d8e69f11994152299e1b0c321c88eff49ebe3d3c
Poup <levejeanchristian@gmail.com>
2020-12-22 12:20:19 +0100

    RAJOUT de MembreEquipeController.php

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+    /**
+     * @Route("/admin", name="admin_")
+     */
+class DashboardController extends AbstractController
+{
+   /**
+     *URL: /admin
+     * nom: admin_dashboard
+     * @Route("", name="dashboard")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
